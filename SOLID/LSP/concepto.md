767b7071ad3835001820fc97071bc830c8e44d2f
gonzalo <gonzalo.sosa@moxit.com.ar>
2024-10-29 14:23:19 -0300

    Principio LSP con ejemplo

---
+++ SOLID/LSP/concepto.md
@@ -0,0 +1,3 @@
+# Liskov Substitution Principle (LSP)
+
+La clase base debe ser capaz de sustituirse por una subclase sin alterar el comportamiento correcto del programa.
--- SOLID/LSP/ejemplo.js
+++ SOLID/LSP/ejemplo.js
@@ -1,7 +1,16 @@
 /*
-Liskov Substitution Principle (LSP):
+Liskov Substitution Principle(LSP):
 
 - Descripción: Las subclases deben poder reemplazar a sus clases base sin alterar el comportamiento correcto del programa. Crea una jerarquía de clases donde las subclases puedan sustituir a la clase base sin romper la funcionalidad.
 
 TODO: Asegúrate de que las subclases puedan ser usadas de manera intercambiable con la clase base sin modificar el comportamiento esperado del programa. Evalúa que el diseño de tu jerarquía de clases mantenga la coherencia con este principio.
 */
+
+import { TextInput } from "./TextInput.js";
+import { NumberInput } from "./NumberInput.js";
+
+const inputs = [new TextInput("hola"), new NumberInput(2)];
+
+for (const input of inputs) {
+  console.log(input, input.validar());
+}
