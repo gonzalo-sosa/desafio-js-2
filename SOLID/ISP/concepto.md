28940c33eb01c522d4dc90b50b62b4ed754efab5
gonzalo <gonzalo.sosa@moxit.com.ar>
2024-10-29 15:44:11 -0300

    Principio ISP con ejemplo de herencia y agregación

---
+++ SOLID/ISP/concepto.md
@@ -0,0 +1,3 @@
+# Interface Segregation Principle (ISP):
+
+Los clientes no deberían verse obligados a depender de interfaces que no utilizan.
--- SOLID/ISP/ejemplo.js
+++ SOLID/ISP/ejemplo.js
@@ -5,3 +5,22 @@ Interface Segregation Principle (ISP):
 
 - TODO: Crea interfaces explícitas o implícitas (a través de funciones o clases en JavaScript) y asegúrate de que cada clase implemente solo los métodos que son relevantes para su función específica. Evita que una clase esté sobrecargada de métodos innecesarios.
 */
+
+import { Pato } from "./Pato.js";
+import { Pajaro } from "./Pajaro.js";
+import { Pescado } from "./Pescado.js";
+
+export const ejemploISP = () => {
+  console.log("\n********** Ejemplo de ISP **********\n");
+
+  const pato = new Pato();
+  const pajaro = new Pajaro();
+  const pescado = new Pescado();
+
+  pescado.nadar();
+
+  pajaro.volar();
+
+  pato.nadar();
+  pato.volar();
+};
